85792f465cf69890593add91d19898793e8ed66f
Katya <katya0897@yandex.ru>
2022-09-18 16:59:20 +0300

    resh

---
+++ z_64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (m>n)
+for (int i = n; i <= m; i++)
+    Console.Write($" {i}");
+else
+    for (int i = m; i <= n; i++)
+        Console.Write($" {i}");
---
+++ z_66/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSum(m, n, temp=0);
+
+void PrintSum(int m, int n, int sum)
+{
+  sum = sum + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {sum} ");
+    return;
+  }
+  PrintSum(m, n - 1, sum);
+}
